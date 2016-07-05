2299e55effccba8a65fc423eee0f7e3304e1e2b7
Bob Marks <marksie531@gmail.com>
2016-07-05 17:43:39 +0100

    - Initial commit from Google code onto GitHub.

---
+++ README.md
@@ -0,0 +1,18 @@
+## BOB LANG
+
+### OVERVIEW
+
+Welcome to Bob Lang.  Run the "boblang.bat" file to execute.  This application
+can also be run as an applet in a webpage.  The XML file can be edited to 
+your own specification / language.
+
+
+# CODE
+
+If you are interested in developing this application I recommmend having both 
+GIT, Eclipse and Maven installed.
+
+
+# CONTACT
+
+Any questions email the author (Bob Marks) at:  marksie531@gmail.com
---
+++ src/main/java/org/bobmarks/lang/other/SpecialFormat.java
@@ -0,0 +1,111 @@
+package org.bobmarks.lang.other;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
+/**
+ * Display words with special symbols in them.
+ *
+ * @author Bob Marks
+ */
+public class SpecialFormat {
+
+    private static final String LANG_FILE = "lang.xml";
+
+    private Element root;
+
+    /**
+     * Constructor.
+     */
+    public SpecialFormat() throws Exception {
+        loadFile();
+
+        showSpecialSymbols();
+    }
+
+    /**
+     * Load file.
+     *
+     * @throws JDOMException
+     * @throws IOException
+     */
+    private void loadFile() throws JDOMException, IOException {
+        File file = new File(LANG_FILE);
+
+        // Parse XML
+        SAXBuilder builder = new SAXBuilder();
+        Document dom = builder.build(file);
+
+        this.root = dom.getRootElement();
+    }
+
+    /**
+     * Load GUI elements.
+     */
+    private void showSpecialSymbols() {
+        display(true);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void display(boolean showHint) {
+        String format = "%1$-100s%2$-10s\n";
+        List<Element> courseElms = root.getChildren("course");
+        for (Element courseElm : courseElms) {
+            String courseName = courseElm.getAttributeValue("name");
+
+            if ("Level 3".equals(courseName)) {
+
+                if (!showHint) {
+                    System.out.println("\nCourse: " + courseName + "\n");
+                }
+                List<Element> lessonElms = courseElm.getChildren("lesson");
+
+                for (Element lessonElm : lessonElms) {
+                    String lessonName = lessonElm.getAttributeValue("name");
+                    if (!showHint) {
+                        System.out.println("\nLesson: " + lessonName + "\n");
+                    }
+
+                    List<Element> topicElms = lessonElm.getChildren("topic");
+
+                    for (Element topicElm : topicElms) {
+                        String topicName = topicElm.getAttributeValue("name");
+                        if (!showHint) {
+                            System.out.println("\nTopic: " + topicName + "\n");
+                        }
+
+                        List<Element> itemElms = topicElm.getChildren("item");
+
+                        for (Element itemElm : itemElms) {
+                            String answer = itemElm.getAttributeValue("a");
+                            String question = itemElm.getAttributeValue("q");
+                            String hint = itemElm.getAttributeValue("h");
+
+                            if (hint != null && !hint.isEmpty() && showHint) {
+                                System.out.format(format, question + " - [ " + hint + " ]", answer);
+                            }
+                            else if (!showHint) {
+                                System.out.format(format, question, answer);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     */
+    public static void main(String [] args) throws Exception {
+        new SpecialFormat();
+    }
+}
